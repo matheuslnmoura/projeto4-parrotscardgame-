ee739aaf0fc736f016f7114ed0d377d75d515064
Matheus Moura <matheuslnmoura@gmail.com>
2022-02-01 02:25:36 -0300

    Interface created and Card Selection Logic Applied.

---
+++ assets/script/cards.js
@@ -0,0 +1,9 @@
+cardsJson = [
+    {id: 0, backImg: "assets/media/bobrossparrot.gif", alt: "Bob Ross Parrot Gif"}, 
+    {id: 1, backImg: "assets/media/explodyparrot.gif", alt: "Explody Parrot Gif"}, 
+    {id: 2, backImg: "assets/media/fiestaparrot.gif", alt: "Fiesta Parrot Gif"}, 
+    {id: 3, backImg: "assets/media/metalparrot.gif", alt: "Metal Parrot Gif"}, 
+    {id: 4, backImg: "assets/media/revertitparrot.gif", alt: "Revert It Parrot Gif"}, 
+    {id: 5, backImg: "assets/media/tripletsparrot.gif", alt: "Triplets Parrot Gif"}, 
+    {id: 6, backImg: "assets/media/unicornparrot.gif", alt: "Unicorn Parrot Gif"}
+]
---
+++ assets/script/script.js
@@ -0,0 +1,108 @@
+
+
+
+
+let numberOfCards = null; 
+let gameArray = [];
+let cardsArray= [];
+let turnedCards = 0;
+let cardVerification = [];
+
+document.querySelector('.start-button').addEventListener('click',()=>{
+    document.querySelector('.start-button').classList.add('no-display');
+    askNumberOfCards();
+});
+
+function askNumberOfCards() {
+      
+    numberOfCards = prompt('EN: How many cards would you like to play with? Choose any even number between 4 and 14. \n\nPORT: Com quantas cartas você gostaria de jogar? Escolha qualquer número par entre 4 e 14.'); 
+
+    if (numberOfCards % 2 == 0 && numberOfCards >= 4 && numberOfCards <= 14) {
+        alert(`
+        EN: Starting game with ${numberOfCards} cards...
+        PORT: Iniciando jogo com ${numberOfCards} cartas...`);
+
+        buildGameArray(numberOfCards/2);
+
+    } else {
+        alert('EN: Choose an even number between 4 and 14.\n\nPORT: Escolha um número par entre 4 e 14.');
+        askNumberOfCards();
+    }
+    
+}
+
+function buildGameArray(cardsNumber) {
+    let slicedArray = cardsJson.slice(0, cardsNumber);
+
+    slicedArray.map((item) => {
+        gameArray.push(item);
+        gameArray.push(item);
+    });
+
+    gameArray = gameArray.sort(comparator);
+
+    createInterface();
+
+}
+
+function comparator() { 
+	return Math.random() - 0.5; 
+}
+
+function createInterface() {
+
+    gameArray.map((item) => {
+        let card = document.querySelector('.card').cloneNode(true);
+
+        card.setAttribute('card-id', item.id)
+        card.querySelector('.back-face img').src = item.backImg;
+        card.querySelector('.back-face img').alt = item.alt;
+
+        document.querySelector('section').append(card);
+
+    });
+
+    cardsArray = Array.from(document.querySelectorAll('section .card'));
+
+    cardSelection();
+
+}
+
+
+
+function cardSelection() {
+    cardsArray.map((item) => {
+        item.addEventListener("click", ()=>{
+            if (cardVerification.length <= 2) {
+                item.querySelector('.front-face').style.transform = 'rotateY(-180deg)';
+                item.querySelector('.back-face').style.transform = 'rotateY(0deg)';
+
+                let cardKey = item.getAttribute('card-id');
+                cardVerification.push(cardKey);
+
+                if (cardVerification.length === 2) {
+                    if(cardVerification[0] === cardVerification[1]) {
+                        console.log('Acertou');
+                        cardVerification.splice(0,2);
+                        
+                    } else {
+                        console.log('Errou');
+                        cardVerification.splice(0,2);
+
+
+                    }
+                }
+                
+            }
+        });
+        
+    });
+}
+
+
+
+
+
+
+
+
